a9df09f74aff6e796c87917c21355e704692c4f2
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-01-05 16:57:56 +0100

    identify the mode of evolution for PC1

--- src/mode_evolution_Pr.R
+++ src/mode_evolution_Pr.R
@@ -1,5 +1,9 @@
 #### Include code for data import ####
+source("src/import_data.R")
+require(dplyr)
+require(paleoTS)
 
+##### 
 pca_data <- data.frame(PC1 = PCA$x[, 1], 
                        PC2 = PCA$x[, 2], 
                        Country = samples$country,
@@ -16,23 +20,55 @@ pca_Pr <- pca_data[pca_data$Section == "Pr",]
 pca_Pr$Rock_sample <- gsub("\\D$", 
                            x = pca_Pr$Rock_sample,
                            replacement = "")
-
-pca_Pr$Rock_sample <- droplevels(pca_Pr$Rock_sample)
-Pr_samples <- unique(pca_Pr$Rock_sample)
-write.csv(Pr_samples,
-          file = "data/Pr_samples.csv",
-          sep = ".",
-          col.names = T)
+pca_Pr$Rock_sample <- as.factor(pca_Pr$Rock_sample)
 
 Pr_heights <- read.csv(file = "data/Prikrnica_heights.csv",
                        header = F)
-colnames(Pr_heights) <- c("Sample", "Height")
+colnames(Pr_heights) <- c("Rock_sample", "Height")
 
 pca_Pr <- pca_Pr[order(pca_Pr$Rock_sample),]
-Pr_heights <- Pr_heights[order(Pr_heights$Sample),]
+Pr_heights <- Pr_heights[order(Pr_heights$Rock_sample),]
 
-pca_Pr_heights <- merge(x = pca_Pr,
-                        y = Pr_heights,
-                        by.x = "Rock_sample",
-                        by.y = "Sample",
-                        all.x = all)
+pca_Pr_heights = dplyr::left_join(x=pca_Pr,
+                            y=Pr_heights,
+                            by="Rock_sample")
+
+PC1_ts <- pca_Pr_heights %>%
+  group_by(Height) %>%
+  summarize(
+    count = n(),
+    mean = mean(PC1, na.rm = TRUE),
+    var = var(PC1, na.rm = TRUE)
+  )
+PC1_ts <- PC1_ts %>% relocate("Height", .after = last_col())
+
+# Plot the morphospace to see if there is any relationship between position 
+# in the section and in the morphospace
+# Point size is proportional to height in the section
+
+plot(pca_Pr$PC1, pca_Pr$PC2,
+     cex = (PC1_ts$Height-5)^(1/3),
+     xlab = "PC1",
+     ylab = "PC2",
+     col = "orange",
+     pch = 16
+)
+
+
+PC1_ts <- PC1_ts[order(PC1_ts$Height),] 
+PC1_ts <- PC1_ts[complete.cases(PC1_ts), ] 
+
+PC1_mode <- paleoTS::as.paleoTS(mm = PC1_ts$mean,
+                     vv = PC1_ts$var,
+                     nn = PC1_ts$count,
+                     tt = PC1_ts$Height,
+                     oldest = "first",
+                     reset.time = T)  
+
+fit9models(PC1_mode, 
+           method = "AD",
+           pool = FALSE)
+
+PC1_sstasis <- opt.joint.Stasis(y = PC1_mode,
+                                pool = F)
+plot(PC1_mode, modelFit = PC1_sstasis)
