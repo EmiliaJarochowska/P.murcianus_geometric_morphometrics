ed187843030e078464b2fef1b8d2569dfa95c747
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-01-05 11:32:11 +0100

    rename variables in the samples file

--- codes_PPT.R
+++ codes_PPT.R
@@ -41,20 +41,20 @@ sliders <- define.sliders(3:138)
 # Assuming `landmarks` is already defined and loaded
 samples <- data.frame(name = rep(NA, dim(landmarks)[3]),
                       country = rep(NA, dim(landmarks)[3]),
-                      variable1 = rep(NA, dim(landmarks)[3]),
-                      variable2 = rep(NA, dim(landmarks)[3]),
-                      variable3 = rep(NA, dim(landmarks)[3]))
+                      section = rep(NA, dim(landmarks)[3]),
+                      rock_sample = rep(NA, dim(landmarks)[3]),
+                      element_number = rep(NA, dim(landmarks)[3]))
 
 for (i in 1:dim(landmarks)[3]) {
   samples$name[i] <- dimnames(landmarks)[[3]][i]
   samples$country[i] <- unlist(strsplit(samples$name[i], "_"))[1]
-  samples$variable1[i] <- unlist(strsplit(samples$name[i], "_"))[2]
-  samples$variable2[i] <- unlist(strsplit(samples$name[i], "_"))[3]
-  samples$variable3[i] <- unlist(strsplit(samples$name[i], "_"))[4]
+  samples$section[i] <- unlist(strsplit(samples$name[i], "_"))[2]
+  samples$rock_sample[i] <- unlist(strsplit(samples$name[i], "_"))[3]
+  samples$element_number[i] <- unlist(strsplit(samples$name[i], "_"))[4]
 }
 
 samples$country <- as.factor(samples$country)
-samples$variable1 <- as.factor(samples$variable1)
+samples$rock_sample <- as.factor(samples$rock_sample)
 
 # Perform Procrustes analysis
 sliders <- define.sliders(3:138)
