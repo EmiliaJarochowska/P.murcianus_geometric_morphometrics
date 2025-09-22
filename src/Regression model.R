cc23c1e3cb4dea8e5b00fc50fb53e36392ad215e
KatjaOselj <katja.oselj@geo-zs.si>
2025-09-22 11:37:48 +0200

    by instruction in 8th chapter

--- src/Regression model.R
+++ src/Regression model.R
@@ -26,28 +26,25 @@ m <- lm(PC1 ~ Length, data = data_combined)
 summary(m)
 
 
-#### Correlation Analysis ####
-# Overall Pearson correlation
+confint(m)
+coef(m)
+ggplot(data_combined, aes(Length, PC1)) +
+  geom_point() +
+  geom_abline(aes(intercept = coef(m)[1], slope = coef(m)[2]),
+              colour = "red")
+
+
+
+#### ANOVA ####
+n  <- aov(PC1 ~ Length, data = data_combined)
+summary(n)
+
+
+
+#### Pearson correlation ####
 cor_test <- cor.test(data_combined$PC1, data_combined$Length)
 cat("Overall Pearson correlation:\n",
     "R =", round(cor_test$estimate, 3),
     "| p-value =", signif(cor_test$p.value, 3), "\n\n")
 
-
-# ANOVA
-anova_overall <- lm(PC1 ~ Length, data = data_combined)
-summary(anova_overall)
-
-# Plot
-ggplot(data_combined, aes(x = Length, y = PC1, color = Region)) +
-  geom_point(alpha = 0.7) +
-  geom_smooth(method = "lm", se = FALSE) +
-  scale_color_manual(values = colors_list$Region) +
-  theme_minimal() +
-  labs(
-    title = "PC1 vs Length by Region",
-    x = "Length [µm]",
-    y = "PC1",
-    color = "Region"
-  )
 
