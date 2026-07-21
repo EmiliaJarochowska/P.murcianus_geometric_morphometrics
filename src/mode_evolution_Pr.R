cf1689fed980dbe00143245f4d51f46650b2484f
KatjaOselj <katja.oselj@geo-zs.si>
2026-07-21 17:11:02 +0200

    Mode_evolution
    Mode of evolution that includes all adult specimens

--- src/mode_evolution_Pr.R
+++ src/mode_evolution_Pr.R
@@ -1,7 +1,100 @@
 #### Include code for data import ####
-source("src/import_data.R")
-require(dplyr)
+
+
 require(paleoTS)
+library(dplyr)
+set.seed(42)
+
+#### Import landmarks and specimen information ####
+
+tps_file = "data/Prikrnica_mode_evolution.TPS"
+
+
+# Import landmarks from TPS file
+landmarks <- geomorph::readland.tps(tps_file, specID = "ID", readcurves = TRUE)
+
+# Import specimen information from CSV
+specimen_info <- read.csv("data/Specimens_info_mode_evolution.csv", header = TRUE)
+
+#### Data cleaning and preparation ####
+
+specimen_info$Region <- trimws(specimen_info$Region)
+
+# Clean specimen IDs: trim whitespace and unify case
+specimen_info$ID <- specimen_info$ID %>% 
+  trimws() %>% 
+  toupper()
+
+tps_ids <- dimnames(landmarks)[[3]] %>% 
+  trimws() %>% 
+  toupper()
+
+dimnames(landmarks)[[3]] <- tps_ids
+
+# Check for unmatched specimen IDs
+missing_in_csv <- tps_ids[!tps_ids %in% specimen_info$ID]
+missing_in_tps <- specimen_info$ID[!specimen_info$ID %in% tps_ids]
+
+if (length(missing_in_csv) > 0) {
+  cat("TPS specimen IDs NOT found in Specimens_info.csv:\n")
+  print(missing_in_csv)
+}
+
+if (length(missing_in_tps) > 0) {
+  cat("Specimens_info.csv IDs NOT found in TPS:\n")
+  print(missing_in_tps)
+}
+
+# Stop if any missing IDs to avoid misalignment
+if (length(missing_in_csv) > 0 || length(missing_in_tps) > 0) {
+  stop("Mismatch between TPS IDs and specimen_info IDs. Please fix and rerun.")
+}
+
+# Match specimen_info order to TPS specimen IDs
+match_rows <- match(tps_ids, specimen_info$ID)
+specimen_info_matched <- specimen_info[match_rows, ]
+rm(tps_ids)
+rm(missing_in_csv)
+rm(missing_in_tps)
+rm(match_rows)
+
+#### Geometric morphometric preprocessing ####
+
+# Define sliding landmarks
+sliders <- geomorph::define.sliders(3:138)
+# Perform Generalized Procrustes Analysis (GPA)
+landmarks.gpa <- geomorph::gpagen(landmarks, curves = sliders)
+
+
+# Perform PCA on GPA-aligned coordinates
+PCA <- geomorph::gm.prcomp(landmarks.gpa$coords)
+plot(PCA)
+
+
+# Calculate mean shape
+msho <- geomorph::mshape(landmarks.gpa$coords)
+
+#### Calculate mean distances and combine data ####
+
+source("src/calculate_distance.R")
+source("src/process_data.R")
+
+lengths <- process_data(tps_file)
+
+# Combine all data
+data_combined <- specimen_info_matched %>%
+  dplyr::left_join(lengths, by = "ID")
+
+# Add PCA scores
+data_combined$PC1 <- PCA$x[, 1]
+data_combined$PC2 <- PCA$x[, 2]
+data_combined$PC3 <- PCA$x[, 3]
+data_combined$PC4 <- PCA$x[, 4]
+data_combined$PC5 <- PCA$x[, 5]
+data_combined$PC6 <- PCA$x[, 6]
+data_combined$PC7 <- PCA$x[, 7]
+
+all_pcs <- PCA$x
 
 samples <- data.frame(name = rep(NA, dim(landmarks)[3]),
                      country = rep(NA, dim(landmarks)[3]),
@@ -81,16 +174,20 @@ PC1_ts <- PC1_ts[order(PC1_ts$Height),]
 PC1_ts <- PC1_ts[complete.cases(PC1_ts), ] 
 
 PC1_mode <- paleoTS::as.paleoTS(mm = PC1_ts$mean,
-                     vv = PC1_ts$var,
-                     nn = PC1_ts$count,
-                     tt = PC1_ts$Height,
-                     oldest = "first",
-                     reset.time = T)  
+                                vv = PC1_ts$var,
+                                nn = PC1_ts$count,
+                                tt = PC1_ts$Height,
+                                oldest = "first",
+                                reset.time = T)  
 
 mode_evolution <- fit9models(PC1_mode, 
-           method = "AD",
-           pool = FALSE)
+                             method = "AD",
+                             pool = FALSE)
 write.csv(mode_evolution, file="supplementary_material/Tab.S4.csv")
+
+plot(PC1_mode)
+
+
 
 # fit a punctuated model from the data
 punc <- fitGpunc(PC1_mode, oshare = FALSE, pool = FALSE)
@@ -101,4 +198,8 @@ dev.off()
 
 plot(PC1_mode, modelFit = punc)
 
+stasis <- fitSimple(PC1_mode, 
+                    model = "Stasis",
+                    pool = FALSE)
 
+plot(PC1_mode, modelFit = stasis)
